59d12b56301ed3a241aa6de45907eea6f451f0d2
Hann0T <hansolivas1@gmail.com>
2022-09-04 14:08:34 -0500

    refactor: using the iterators methods.

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,22 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next(); // discard the first argument, because the first argument is the path to our program
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get the query string"),
+        };
+
+        let file_name = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
 
         Ok(Config {
-            query: args[1].clone(),
-            file_name: args[2].clone(),
+            query,
+            file_name,
             case_sensitive: env::var("CASE_INSENSITIVE").is_err(),
         })
     }
@@ -37,15 +45,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
